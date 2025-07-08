9bfaa46a3e3129d0fb92f4d96cbe1e27c9eb7e3a
Stacey-M <sincerely.staymi>
2025-07-08 12:03:10 -0700

    Haunted House

--- app2.py
+++ app2.py
@@ -1,1 +1,8 @@
-print("app2")
+print("lets make a fun haunted house story!")
+Adj1 = input ("give me a adjective?")
+PNoun1 = input ("give me a plural noun!")
+Name = input("how about a name now?")
+PNoun2 = input ("Another plural noun last one!")
+Adj2 = input ("Just kidding one more adjective pretty please")
+#story will be played now
+print("On a" + Adj1 + " " + "dark night, a group of" + PNoun1 + " " + "decided to explore the mysterious haunted house at the edge of town. As they entered, they felt a cold chill run down their spines. Creaky" + PNoun2 + " " + "echoed through the halls as they cautiously made their way through the dimly lit rooms. Suddenly, a ghostly" + Name + " " + "appeared before them, sending them running out of the" + Adj2 + " " + "house in a state of pure terror.")
